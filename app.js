71dbfc3ceccfbc1f6ca59f70540330f026ddf865
pi342us-g <ndubipius96@gmail.com>
2025-07-07 20:54:59 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// import express
+const express = require("express");
+
+
+// import the mongoose module that will help you connect with database
+const mongoose = require('mongoose');
+
+// import the dotenv module
+require('dotenv').config()
+
+// create an express application
+const app = express();
+// specify the data format you expect to receive the data in
+app.use(express.json());
+
+
+// connect to the database
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connection succesfull")).catch((err) => console.error("Error connecting to the MongoDb",err));
+
+// specify the port the application will  be running
+const PORT = process.env.PORT|| 3000;
+
+
+// listen to the port
+app.listen(PORT,()=>{
+    console.log("The server is running on port "+ PORT)
+});
+
+// setup project of your own with atleast 3 schema and connect to online and local db
---
+++ models/model.js
@@ -0,0 +1,59 @@
+// what is a schema
+// a schema usually specifies the different details you expect to have for your different 
+// collectoins in your database
+
+// import the mongoose library that will help you connect with mongodb
+const mongoose =require('mongoose');
+
+// access the schema package/library inside of mongoose
+const Schema = mongoose.Schema;
+
+// we shall create three collections i.e userSchema, employeeSchema,departmentSchema
+
+//userSchema
+const userSchema = new Schema({
+    name : String,
+    email : {type: String,required: true,unique : true},
+    dob: {type: Date,default : Date.now},
+    password : {type: String,required:true},
+    photo:String,
+});
+
+// employeeSchema
+const employeeSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: "User", default: null },
+  firstName: String,
+  lastName: String,
+  email: { type: String, required: true, unique: true },
+  departmentId: {
+    type: Schema.Types.ObjectId,
+    ref: "Department",
+    required: true,
+  },
+  jobTitle: String,
+  hireDate: Date,
+  salary: Number,
+  status: {
+    type: String,
+    enum: ["active", "inactive", "on_leave"],
+    default: "active",
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+// departmentSchema
+const departmentSchema = new Schema({
+    name:{Type:String,required:true,unique:true},
+    description:String,
+    managerId:{type:Schema.Types.ObjectId,ref:'Employee',default:null},
+    createdAt:{type:Date,default:Date.now},
+    updateAt:{type:Date,default:Date.now},
+});
+// make them accessible throughout the application by exporting them
+const User = mongoose.model("User",userSchema);
+const Employee = mongoose.model("Employee",employeeSchema);
+const Department = mongoose.model("Department",departmentSchema);
+
+// Export all of them
+module.exports = {User,Employee,Department};
