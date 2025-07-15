228d57f96d4bb3d8d5962aafcbf41bd7aa4bab5c
pi342us-g <ndubipius96@gmail.com>
2025-07-15 11:47:44 +0300

    added files

--- routes/employees.js
+++ routes/employees.js
@@ -34,6 +34,49 @@ router.post("/",async (req,res) => {
 })
 
 
+// Below route is for viewing all employees available
+// Get all employee
+router.get("/",async (req,res) => {
+    try{
+        // find all employees and put them in a variable
+        const employees =await Employee.find().populate('userId','email name photo').populate('departmentId','name');
+        res.json(employees);
+    }
+    catch(error){
+        res.status(500).json({message:"Failed to fetch all employees",Error:err.message})
+    }
+    
+})
 
+// fetch a single employee based on the Id of the employee
+router.get("/:id",async(req,res)=>{
+    try {
+        // test whether id exists exists on a database
+        const employee = await Employee.findById(req.params.id)
+        if(!employee){
+            return res.status(404).json({message:"User not found"})
+        }
+        //if employee exists show details
+        res.json(employee)
+    } catch (error) {
+        res.status(500).json({error:err.message})
+    }
+});
+// delete employee
+router.delete("/:id",async(req,res)=>{
+    try{
+        // fetch the particular employee id
+        const employee = await Employee.findByIdAndDelete(req.params.id)
+        // check whether there is an employee with the given id
+        if(!employee){
+            return res.status(404).json({message:"Employee not found"})
+        }
+        // if the id is correct we should proceed by deleting the employee
+        res.json({message:"Employee deleted successfully"})
+    }
+    catch(error){
+        res.status(400).json({error:err.message})
+    }
+})
 
 module.exports = router;
